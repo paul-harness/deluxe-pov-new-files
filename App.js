749fc8758303fdbc4d309c7b9d8544eff4eaf141
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-12 06:59:59 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css';
 import { Header } from './components/Header'
+import { Users } from './components/Users'
 import CreateUser from './components/CreateUser'
+import { DisplayBoard } from './components/DisplayBoard'
 import { getAllUsers, createUser } from './services/UserService'
 
 class App extends Component {
@@ -20,6 +22,14 @@ class App extends Component {
         this.setState({numberOfUsers: this.state.numberOfUsers + 1})
     });
   } 
+
+  getAllUsers = () => {
+    getAllUsers()
+      .then(users => {
+        console.log(users)
+        this.setState({users: users, numberOfUsers: users.length})
+      });
+  }
 
   onChangeForm = (e) => {
     let user = this.state.user
@@ -47,7 +57,17 @@ class App extends Component {
                   >
                 </CreateUser>
             </div>
+            <div className="col-md-4">
+                <DisplayBoard
+                  numberOfUsers={this.state.numberOfUsers}
+                  getAllUsers={this.getAllUsers}
+                >
+                </DisplayBoard>
+            </div>
           </div>
+        </div>
+        <div className="row mrgnbtm">
+          <Users users={this.state.users}></Users>
         </div>
       </div>
     );
